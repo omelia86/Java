2d902ae1dd818c94f255a82bd57930a4ef0ae4c0
omelia86 <gulnaz.umitbaeva@gmail.com>
2023-11-19 20:06:17 +0300

    first examples were added

---
+++ Chapter1/Example2/Example2.java
@@ -0,0 +1,19 @@
+/*
+	Демонстрация использования переменных.
+	Назовите этот файл Example2.java.
+*/ 
+	class Example2 {
+		public static void main (String[] args) {
+			int myVar1; // объявление переменной 
+			int myVar2; // объявление еще одной переменной 
+			
+			myVar1 = 1024; // присваивание переменной myVar1 значения 1024
+		
+		
+		System.out.println("myVar1 содержит " + myVar1);
+			myVar2 = myVar1/2;
+			
+			System.out.print("myVar2 содержит myVar1/2: ");
+			System.out.println(myVar2);
+			} 	
+		}	
---
+++ Chapter1/GalToLit/GalToLit.java
@@ -0,0 +1,9 @@
+class GalToLit {
+	public static void main(String[] args) {
+		double gallons; // хранит количество галонов
+		double liters; // хранит результат преобразования в литры
+		gallons=10; // начать с 10 галонов
+		liters=gallons*3.7854; // преобразование в литры
+		System.out.println(gallons + "галонов соответсвует" +liters + "литрам.");
+		} 
+	}
