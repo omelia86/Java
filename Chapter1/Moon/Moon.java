6a9035e46e4f12235a164c09fd9aca5a316b34ee
omelia86 <gulnaz.umitbaeva@gmail.com>
2023-11-21 20:03:02 +0300

    Moon

---
+++ Chapter1/Moon/Moon.java
@@ -0,0 +1,42 @@
+class Moon {
+	public static void main(String[] args) {
+		double earthweight; // вес на земле
+		double moonweeight; // вес на луне
+		earthweight = 65.0; 
+		moonweeight = earthweight*0.17;
+		System.out.println("Мой вес на луне составляет " + moonweeight + "кг"
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		);
+		} 
+	}
