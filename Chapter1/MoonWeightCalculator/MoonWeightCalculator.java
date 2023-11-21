61624958aef2dc1d68eabe5feaa1a4f2baa4c1d1
omelia86 <gulnaz.umitbaeva@gmail.com>
2023-11-21 21:02:33 +0300

    Moon changed to MoonWeightCalculator

---
+++ Chapter1/MoonWeightCalculator/MoonWeightCalculator.java
@@ -0,0 +1,9 @@
+class MoonWeightCalculator {
+	public static void main(String[] args) {
+		double earthweight; // вес на земле
+		double moonweeight; // вес на луне
+		earthweight = 143.3; 
+		moonweeight = earthweight*0.17;
+		System.out.println("Мой вес на луне составляет " + moonweeight + " фунтов");
+	}
+}
